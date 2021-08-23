36920888cb3ef8353cbabf8c6080d076dd6407f8
micheals <micheals@atguigu.com>
2021-08-23 18:16:54 +0800

    flis commit

--- src/main/java/com/micheal/Mainstudent.java
+++ src/main/java/com/micheal/Mainstudent.java
@@ -9,5 +9,8 @@ public class Mainstudent {
     public static void main(String[] args) {
         System.out.println("你好欢迎来Git管理......");
         System.out.println("asdsadasdasdasd");
+        System.out.println("asdashdkadhasjdhsjakdhajksd");
+        System.out.println("sadhasjdhaskdhajshdaskdjhjakshdkasd");
+        System.out.println("dasdasjkdhasjkdhaskjdhakdhkashdkashdkadhkas");
     }
 }
