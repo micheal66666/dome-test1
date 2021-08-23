c0ea0739618492e9152bc362241029ad3c9f668d
micheals <micheals@atguigu.com>
2021-08-23 18:06:24 +0800

    first commit

---
+++ src/main/java/com/micheal/Mainstudent.java
@@ -0,0 +1,13 @@
+package com.micheal;
+
+/**
+ * @author ：micheal
+ * @description：TODO
+ * @date ：Created in 2021/8/23 18:01
+ */
+public class Mainstudent {
+    public static void main(String[] args) {
+        System.out.println("你好欢迎来Git管理......");
+        System.out.println("asdsadasdasdasd");
+    }
+}
