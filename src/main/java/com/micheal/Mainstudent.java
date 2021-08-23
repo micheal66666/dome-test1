db6e509b5122bec6e1243d3845a074a4e28a863b
micheals <micheals@atguigu.com>
2021-08-23 18:09:41 +0800

    version commit

--- src/main/java/com/micheal/Mainstudent.java
+++ src/main/java/com/micheal/Mainstudent.java
@@ -9,5 +9,6 @@ public class Mainstudent {
     public static void main(String[] args) {
         System.out.println("你好欢迎来Git管理......");
         System.out.println("asdsadasdasdasd");
+        System.out.println("dasdadasdasdasdasdasdadsad");
     }
 }
